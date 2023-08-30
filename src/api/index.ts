d73958eb59b1bec7a597ace767876dfa2992a4cf
zachj95 <zachariejones@gmail.com>
2023-08-30 16:36:05 -0700

    Update cors, remove get from api index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,7 @@
 import { Router } from "express";
 import stats from "./stats/stats";
-import { GAMES } from "../sampleData";
-import { parsePgn } from "./utils";
 
 const router = Router();
-
-router.get<any, any>("/", async (req, res) => {
-  res.json(parsePgn(GAMES[0].pgn));
-});
 
 router.use("/stats", stats);
 
