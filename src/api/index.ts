c2daffb0c611998fa00956674c41fb3e086e5b65
zachj95 <zachariejones@gmail.com>
2023-08-31 13:09:27 -0700

    Add GET route to stats index

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,10 @@ import stats from "./stats/stats";
 
 const router = Router();
 
+router.get<any, any>("/", async (req, res) => {
+  res.json({ message: "ChessRecap Stats API" });
+});
+
 router.use("/stats", stats);
 
 export default router;
